aca59a66bc024edc4b5bf9b9b8296f041a7ab05c
kevaldeepak <keval_d@icloud.com>
2020-12-01 21:38:20 +0000

    Add files via upload

---
+++ Election Project - Tia-Keval/src/election/project/tia/keval/ElectionProjectTiaKeval.java
@@ -0,0 +1,15 @@
+package election.project.tia.keval;
+
+/**
+ *
+ * @author Tia - [] / Keval - [21440808]
+ * 
+ * Election Project
+ */
+public class ElectionProjectTiaKeval {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
